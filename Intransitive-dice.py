4e26590707716aaaac63a59386e3546337d092b7
IanLampic <lampic.ian@gmail.com>
2022-12-11 20:55:55 +0100

    Dodal generator seznamov in glavno funkcijo

--- Intransitive-dice.py
+++ Intransitive-dice.py
@@ -1,6 +1,6 @@
-def intranzitivnost(seznam):
+def intransitive(lists):
     lst = []
-    for list in seznam:
+    for list in lists:
         for element in list:
             lst.append(element)
     p1, p2, p3 = 0, 0, 0
@@ -16,4 +16,31 @@ def intranzitivnost(seznam):
 
 testni_primer = [[2, 2, 4, 4, 9, 9], [1, 1, 6, 6, 8, 8], [3, 3, 5, 5, 7, 7]]
 
-print(intranzitivnost(testni_primer) == 5/9)
+import random
+
+def generator_list(m):
+    lst = []
+    random_num = None
+    while len(lst) < 6:
+        random_num = random.randint(1, m)
+        if random_num not in lst:
+            lst.append(random_num)
+        else:
+            continue
+    return lst
+        
+def glavna(n, m): 
+    lst = []
+    while len(lst) < m:
+        first = generator_list(n)
+        second = generator_list(n)
+        third = generator_list(n)
+        lst.append((intransitive(([first, second, third])), [first, second, third]))
+    maxp = 0
+    for i in range(len(lst)):
+        maxp = max(maxp, lst[i][0])
+        lst_with_max = lst[i][1]
+    return maxp, lst_with_max
+
+for i in range(100):
+   print(f"{int(list(zip(glavna(100000000, 10000)))[0][0] * 36)}" + "/36")
